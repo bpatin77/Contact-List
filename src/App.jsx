c0f6ae429294f2260605366f5abf8f00fd05f1c6
Blake <bpatin77@gmail.com>
2024-09-26 22:41:36 -0500

    Contact List Attempted

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 import "./App.css";
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>
+      {selectedContactId ? (
+        <SelectedContact
+          setSelectedContactId={setSelectedContactId}
+          selectedContactId={selectedContactId}
+        />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
+    </>
+  );
+}
